861f4fa19111b806fdc973455f8abbee4d1ac5c6
uselgithub <uselgithub@gmail.com>
2020-09-10 15:12:06 +0800

    updated for new HC-05

--- bt_init_HC05.py
+++ bt_init_HC05.py
@@ -1,9 +1,21 @@
 import serial
 
-for i in range(int(input("Starting unit: ")),9999):
-	input("")
-	with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
-		ser.write(f"AT+NAMEA{str(i).zfill(4)}\r\n".encode())
-		print(ser.read(10).decode())
-		print(f"Device A{str(i).zfill(4)} finished")
+letter = input("Prefix letter: ").upper()
 
+for i in range(int(input("Starting unit: ")),9999):
+        input("")
+        with serial.Serial('/dev/ttyUSB0', 38400, timeout=1) as ser:
+                print("Setting Device Name...")
+                ser.write(f"AT+NAME={letter}{str(i).zfill(4)}\r\n".encode())
+                print(ser.read(10).decode())
+                print("Setting Device PIN...")
+                ser.write(b"AT+PSWD=0000\r\n")
+                print(ser.read(10).decode())
+                print("Setting Device Baud Rate...")
+                ser.write(b"AT+UART=9600,0,0\r\n")
+                ser.write(b"AT+BAUD4\r\n")
+                print(ser.read(10).decode())
+                print("Setting Device Role...")
+                ser.write(b"AT+ROLE=0\r\n")
+                print(ser.read(10).decode())
+                print(f"Device {letter}{str(i).zfill(4)} finished")
--- bt_init_HC06.py
+++ bt_init_HC06.py
@@ -7,6 +7,7 @@ for i in range(int(input("Starting unit: ")),9999):
         with serial.Serial('/dev/ttyUSB0', 38400, timeout=1) as ser:
                 print("Setting Device Name...")
                 ser.write(f"AT+NAME={letter}{str(i).zfill(4)}\r\n".encode())
+                ser.write(f"AT+NAME{letter}{str(i).zfill(4)}\r\n".encode())
                 print(ser.read(10).decode())
                 print("Setting Device PIN...")
                 ser.write(b"AT+PIN=0000\r\n")
@@ -14,9 +15,9 @@ for i in range(int(input("Starting unit: ")),9999):
                 print(ser.read(10).decode())
                 print("Setting Device Baud Rate...")
                 ser.write(b"AT+UART=9600,0,0\r\n")
+                ser.write(b"AT+BAUD4\r\n")
                 print(ser.read(10).decode())
                 print("Setting Device Role...")
                 ser.write(b"AT+ROLE=0\r\n")
                 print(ser.read(10).decode())
                 print(f"Device {letter}{str(i).zfill(4)} finished")
-
